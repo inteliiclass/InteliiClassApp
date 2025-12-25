868975d34e4004e9b593b3de1a77ec17310411ba
OmarBK1 <obasharkhader@gmail.com>
2025-12-25 13:19:22 +0200

    Instructor Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:inteliiclass/screens/instructorLogin.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        inputDecorationTheme: InputDecorationThemeData(
+          labelStyle: GoogleFonts.poppins(color: Colors.white),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+          iconColor: Colors.grey,
         ),
+        fontFamily: "GoogleFonts.poppins",
+        //focusColor: Colors.blue,
+        textTheme: GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.white,displayColor: Colors.white),
+        scaffoldBackgroundColor: const Color.fromARGB(255, 2, 20, 34),
       ),
+      home: const MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: DoctorSignIn());
   }
 }
