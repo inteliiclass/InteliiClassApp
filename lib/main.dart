972df28b40b1cc75e8adf3fbe068c78b7cd28e8a
OmarBK1 <obasharkhader@gmail.com>
2025-12-27 01:32:02 +0200

    Introducing Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:inteliiclass/screens/instructorLogin.dart';
 import 'package:inteliiclass/screens/instructordashborad.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -12,8 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => MainApp(),
-        '/instructorlogin': (context) => Instructorlogin(),
+        '/': (context) => Instructorlogin(),
         '/instructordashborad': (context) => Instructordashborad(),
       },
       theme: ThemeData(
@@ -36,13 +41,5 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color.fromARGB(255, 2, 20, 34),
       ),
     );
-  }
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Instructorlogin());
   }
 }
