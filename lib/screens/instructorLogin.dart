7382f437627e6fcbe248803a5ead6372abda2478
Omar Bashar Khader <obasharkhader@gmail.com>
2025-12-27 01:34:16 +0200

    Introducing Firebase (#7)

--- lib/screens/instructorLogin.dart
+++ lib/screens/instructorLogin.dart
@@ -12,109 +12,140 @@ class _SignInState extends State<Instructorlogin> {
   final key = GlobalKey<FormState>();
   String pass = "";
   bool obscure = true;
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Center(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(0, 40, 0, 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  color: Color.fromARGB(255, 4, 48, 85),
-                ),
-                child: Image.asset(
-                  "images/logo.png",
-                  width: 140,
-                  height: 140,
-                  fit: BoxFit.fill,
-                  color: Colors.blue,
-                ),
-              ),
-              Text(
-                "IntelliClass",
-                style: GoogleFonts.poppins(color: Colors.white, fontSize: 50),
-              ),
-              Text(
-                "Instructor Portal",
-                style: GoogleFonts.poppins(color: Colors.grey, fontSize: 20),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-          child: Form(
+    return Scaffold(
+      body: ListView(
+        children: [
+          Center(
             child: Column(
               children: [
-                ListTile(
-                  title: TextFormField(
-                    style: TextStyle(color: Colors.grey),
-                    cursorColor: Colors.white,
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      hintText: "instructor@gmail.com",
-                      icon: Icon(Icons.email),
-                    ),
-                  ),
-                ),
-                ListTile(
-                  title: TextFormField(
-                    style: TextStyle(color: Colors.grey),
-                    obscureText: obscure,
-                    obscuringCharacter: "*",
-                    cursorColor: Colors.white,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      hintText: "***********",
-                      icon: Icon(Icons.password),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            obscure = !obscure;
-                          });
-                        },
-                        icon: Icon(Icons.visibility),
-                      ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        pass = value;
-                      });
-                    },
-                  ),
-                ),
                 Container(
-                  alignment: Alignment.bottomRight,
-                  child: TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Forget Password?",
-                      style: GoogleFonts.poppins(color: Colors.white),
-                    ),
+                  margin: EdgeInsets.fromLTRB(0, 40, 0, 20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Color.fromARGB(255, 4, 48, 85),
+                  ),
+                  child: Image.asset(
+                    "images/logo.png",
+                    width: 140,
+                    height: 140,
+                    fit: BoxFit.fill,
+                    color: Colors.blue,
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context ,'/instructordashborad');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.blue),
-                      foregroundColor: WidgetStatePropertyAll(Colors.white),
-                      fixedSize: WidgetStatePropertyAll(Size(370, 65)),
-                    ),
-                    child: Text("Log In"),
-                  ),
+                Text(
+                  "IntelliClass",
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 50),
+                ),
+                Text(
+                  "Instructor Portal",
+                  style: GoogleFonts.poppins(color: Colors.grey, fontSize: 20),
                 ),
               ],
             ),
           ),
-        ),
-      ],
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Form(
+              key: key,
+              child: Column(
+                children: [
+                  ListTile(
+                    title: TextFormField(
+                      controller: emailcontroller,
+                      style: TextStyle(color: Colors.grey),
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "instructor@gmail.com",
+                        icon: Icon(Icons.email),
+                      ),
+
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter a Valid Email';
+                        }
+                        if (!RegExp(
+                          r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                        ).hasMatch(value)) {
+                          return 'Enter a valid email address';
+                        }
+                        return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          emailcontroller.text = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: TextFormField(
+                      controller: passwordcontroller,
+                      style: TextStyle(color: Colors.grey),
+                      obscureText: obscure,
+                      obscuringCharacter: "*",
+                      cursorColor: Colors.white,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        hintText: "***********",
+                        icon: Icon(Icons.password),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              obscure = !obscure;
+                            });
+                          },
+                          icon: Icon(Icons.visibility),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Enter your Password";
+                        } else
+                          return null;
+                      },
+                      onChanged: (value) {
+                        setState(() {
+                          pass = value;
+                          passwordcontroller.text = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Forget Password?",
+                        style: GoogleFonts.poppins(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/instructordashborad');
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                        foregroundColor: WidgetStatePropertyAll(Colors.white),
+                        fixedSize: WidgetStatePropertyAll(Size(370, 65)),
+                      ),
+                      child: Text("Log In"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
